89adbbb11a171ca6932063d8c4cef639a6d2d464
jesus <angelhernan12486@gmail.com>
2025-03-19 12:25:29 -0600

    dos

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    image_names = ["imagen_1.png", "imagen_2.png", "imagen_3.png", "imagen_4.png", "imagen_5.png"]
+    image_names = ["imagen_3.png", "imagen_4.png", "imagen_5.png"]
     return render_template('index.html', image_names=image_names)
 
-# Ruta para servir imágenes desde la carpeta static
+# Servir imágenes desde la carpeta static/img
 @app.route('/static/img/<path:filename>')
 def serve_image(filename):
     return send_from_directory('static/img', filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Servir el archivo CSV
+@app.route('/data_mask.csv')
+def serve_csv():
+    return send_from_directory('.', 'data_mask.csv')
+
+if __name__ == "__main__":
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
